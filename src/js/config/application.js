8b1eccb3e0164dd3773405b9688da66b8c5350a6
Calvin Lai <callai@gmail.com>
2013-07-17 20:51:02 -0400

    initial src directory structure

---
+++ src/js/config/application.js
@@ -0,0 +1,6 @@
+var _APP_NAME_    = 'yourappname'
+  , _CONTROLLERS_ = _APP_NAME_ + '.controllers'
+  , _DIRECTIVES_  = _APP_NAME_ + '.directives'
+  , _FILTERS_     = _APP_NAME_ + '.filters'
+  , _MODULES      = _APP_NAME_ + '.modules'
+  , _SERVICES_    = _APP_NAME_ + '.services';
