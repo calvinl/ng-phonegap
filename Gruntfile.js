8d54f6e6622b4e808f7b0de7c514a0038831dfb1
Calvin Lai <calvin@wunwun.com>
2014-06-05 11:54:39 -0400

    upgrade to angular 1.2.16, fontawesome 4, fix Gruntfile for correct path to fontawesome fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,7 +125,7 @@ module.exports = function(grunt) {
         src: ['<%= appDir %>/img']
       },
       img: {
-        src: ['<%= appDir %>/font']
+        src: ['<%= appDir %>/fonts']
       },
       partials: {
         src: ['<%= appDir %>/html/partials']
@@ -159,7 +159,7 @@ module.exports = function(grunt) {
       },
       fonts: {
         files: [
-          { expand: true, cwd: '<%= bowerDir %>/font-awesome/font/', src: ['**'], dest: '<%= appDir %>/font/' }
+          { expand: true, cwd: '<%= bowerDir %>/font-awesome/fonts/', src: ['**'], dest: '<%= appDir %>/fonts/' }
         ]
       },
       tmp_to_build: {
@@ -292,7 +292,7 @@ module.exports = function(grunt) {
         tasks: ['clean:img', 'copy:img']
       },
       fonts: {
-        files: ['<%= srcDir %>/font/**'],
+        files: ['<%= srcDir %>/fonts/**'],
         tasks: ['clean:fonts', 'copy:fonts']
       },
       partials: {
